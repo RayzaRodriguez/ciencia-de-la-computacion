be0b47dd8409597747897873a94436d976066588
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 12:55:50 -0500

    first commit

---
+++ prac.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+/*int main()
+{
+    float a,r;
+    cout <<"ingrese el radio del circulo: "<<endl;
+    cin>>r;
+    a=3.14*r*r;
+    cout<<"el area de circulo es: "<<a<<endl;
+    return 0;
+}*/
+
+/*int main()
+{
+    float a,l;
+    cout <<"ingrese el lado del cuadrado: ";
+    cin>>l;
+    a=l*l;
+    cout<<"el area de cuadrado es: "<<a;
+    return 0;
+}
+
+int main()
+{
+    float a,al,b;
+    cout <<"ingrese la base del rectangulo: ";
+    cin>>b;
+    cout <<"ingrese la altura del rectangulo: ";
+    cin>>al;
+    a=b*al;
+    cout<<"el area de rectangulo es: "<<a<<endl;
+    return 0;
+}
+
+main()
+{
+  int a=3, b=5,temp;
+  cout<<a<<" "<<b<<endl;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<a<<" "<<b;
+}
+
+main()
+{
+    int a=5,b=3;
+    cout<<a<<" "<<b<<endl;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<a<<" "<<b<<endl;
+}
+
+main()
+{
+    int a,b;
+    cout<<"Ingrese valor primero: ";
+    cin>>a;
+    cout<<"Ingrese valor segundo: ";
+    cin>>b;
+    if (a>b)
+        cout<<"el numero mayor es: "<<a;
+    else
+        cout<<"el numero mayor es: "<<b;
+}
+
+main()
+{
+    int a,b,c;
+    cout<<"Ingrese valor primero: ";
+    cin>>a;
+    cout<<"Ingrese valor segundo: ";
+    cin>>b;
+    cout<<"Ingrese valor tercero: ";
+    cin>>c;
+    if (a>b)
+        if(a>c)
+            cout<<"el numero mayor es: "<<a;
+        else
+            cout<<"el numero mayor es: "<<c;
+    else
+        if(b>c)
+            cout<<"el numero mayor es: "<<b;
+        else
+            cout<<"el numero mayor es: "<<c;
+
+}
+
+main()
+{
+    int a,b,c;
+    cout<<"Ingrese valor primero: ";
+    cin>>a;
+    cout<<"Ingrese valor segundo: ";
+    cin>>b;
+    cout<<"Ingrese valor tercero: ";
+    cin>>c;
+    if (a>b&&a>c)
+        cout<<"el numero mayor es: "<<a;
+    else if (b>a&&b>c)
+        cout<<"el numero mayor es: "<<b;
+    else
+        cout<<"el numero mayor es: "<<c;
+
+}*/
+
+
